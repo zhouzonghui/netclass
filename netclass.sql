38f1dbc49b77f0fe3d01ebef668f6ba82f5f81e3
zhouzonghui <zonghui_zhou@foxmail.com>
2015-04-23 10:41:02 +0800

    （数据库脚本文件），除了学生表，其余表的id都设为了自动递增

---
+++ netclass.sql
@@ -0,0 +1,131 @@
+-- MySQL dump 10.13  Distrib 5.6.23, for Win32 (x86)
+--
+-- Host: localhost    Database: netclass
+-- ------------------------------------------------------
+-- Server version	5.6.23-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `t_class`
+--
+
+DROP TABLE IF EXISTS `t_class`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_class` (
+  `c_id` int(2) NOT NULL AUTO_INCREMENT,
+  `c_name` varchar(20) NOT NULL,
+  `t_time` int(2) NOT NULL,
+  PRIMARY KEY (`c_id`),
+  KEY `c_id` (`c_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_class`
+--
+
+LOCK TABLES `t_class` WRITE;
+/*!40000 ALTER TABLE `t_class` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_class` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_notice`
+--
+
+DROP TABLE IF EXISTS `t_notice`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_notice` (
+  `n_id` int(10) NOT NULL AUTO_INCREMENT,
+  `n_info` varchar(2047) NOT NULL,
+  PRIMARY KEY (`n_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_notice`
+--
+
+LOCK TABLES `t_notice` WRITE;
+/*!40000 ALTER TABLE `t_notice` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_notice` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_question`
+--
+
+DROP TABLE IF EXISTS `t_question`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_question` (
+  `q_id` int(11) NOT NULL AUTO_INCREMENT,
+  `q_title` varchar(30) NOT NULL,
+  `q_content` varchar(2047) NOT NULL,
+  `q_student_id` varchar(20) NOT NULL,
+  PRIMARY KEY (`q_id`),
+  KEY `fk2` (`q_student_id`),
+  CONSTRAINT `fk2` FOREIGN KEY (`q_student_id`) REFERENCES `t_student` (`s_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_question`
+--
+
+LOCK TABLES `t_question` WRITE;
+/*!40000 ALTER TABLE `t_question` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_question` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_student`
+--
+
+DROP TABLE IF EXISTS `t_student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_student` (
+  `s_id` varchar(20) NOT NULL,
+  `s_name` varchar(20) NOT NULL,
+  `s_password` varchar(40) NOT NULL,
+  `s_gender` varchar(1) NOT NULL,
+  `s_score` float(4,1) DEFAULT '0.0',
+  `s_class_id` int(2) NOT NULL,
+  PRIMARY KEY (`s_id`),
+  KEY `fk1` (`s_class_id`),
+  CONSTRAINT `fk1` FOREIGN KEY (`s_class_id`) REFERENCES `t_class` (`c_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_student`
+--
+
+LOCK TABLES `t_student` WRITE;
+/*!40000 ALTER TABLE `t_student` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_student` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-04-23 10:35:41
